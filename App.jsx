9b11f9aaa06159608cbc9c7646d37255500bb3ee
josehernandezc380-crypto <josehernandezc380@gmail.com>
2026-08-16 04:32:48 +0200

    Delete App.jsx

--- App.jsx
+++
@@ -1,8 +0,0 @@
-import Login from './Login.jsx'
-
-export default function App() {
-  const handleLogin = (session) => {
-    console.log('Login exitoso:', session)
-  }
-  return <Login onLogin={handleLogin} />
-}
